e26e1a480a47aa3934979097ab1186c914098252
zhu <zhu_huang@126.com>
2020-08-26 23:42:02 +0800

    调节 topnav 和 aside 的位置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>阿轮 UI</h1>
       <h2>一个厉害的 UI 框架</h2>
       <p class="actions">
-        <a>Github</a>
-        <a>开始</a>
+        <a href="https://github.com/qinggge/vue3">Github</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
