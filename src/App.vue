bcde66ac5a1afa89d761bf7736810c2c1be32ef2
zhu <zhu_huang@126.com>
2020-08-26 23:29:13 +0800

    搭建html框架

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <template>
-  <div>导航栏 | <router-link to="/">Qing</router-link> | <router-link to="/xxx">Qing2</router-link></div>
-  <hr>
   <router-view/>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import { createWebHashHistory, createRouter } from 'vue-router'
-import Qing from './components/Qing.vue'
-import Qing2 from './components/Qing2.vue'
+import Home from './views/Home.vue'
+import Doc from './views/Doc.vue'
 
 const history = createWebHashHistory();
 const router = createRouter({
   history: history,
   routes: [
-    {path: '/', component: Qing},
-    {path: '/xxx', component: Qing2},
+    {path: '/', component: Home},
+    {path: '/doc', component: Doc},
   ]
 });
 
