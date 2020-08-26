d01db04314dc9c62be90fb3241e3b29b749648d9
zhu <zhu_huang@126.com>
2020-08-26 23:00:55 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Qing/>
+</template>
+
+<script>
+import Qing from './components/Qing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Qing,
+  }
+}
+</script>
