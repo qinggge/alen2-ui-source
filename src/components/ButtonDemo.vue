471ecd8262fd1451f4fbeb9f01dfb5966ebfa137
zhu <zhu_huang@126.com>
2020-09-15 22:11:06 +0800

    添加 theme 属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -3,11 +3,22 @@
     Button 示例
   </div>
   <h1>示例1</h1>
-  <div><Button>你好</Button></div>
+  <div>
+    <Button>你好</Button>
+    <Button theme="button">你好</Button>
+    <Button theme="link">你好</Button>
+    <Button theme="text">你好</Button>
+  </div>
 </template>
 <script lang="ts">
 import Button from '../lib/Button.vue'
 export default {
   components: { Button },
+  setup() {
+    const onClick = () => {
+      console.log('你好');
+    }
+    return { onClick };
+  }
 }
 </script>
