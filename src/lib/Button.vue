d3ac3e1645128d24022cd91c3207c6a09076f0a2
zhu <zhu_huang@126.com>
2020-09-15 22:45:02 +0800

    支持 disabled

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="alen-button" :class="classes">
+  <button class="alen-button" :class="classes" :disabled="disabled">
     <slot />
   </button>
 </template>
@@ -14,14 +14,23 @@ export default {
     size: {
       type: String,
       default: 'normal',
-    }
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`alen-theme-${theme}`]: theme,
         [`alen-size-${size}`]: size,
+        [`alen-level-${level}`]: level,
       };
     });
     return { classes };
@@ -33,6 +42,8 @@ $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
+$red: red;
+$grey: grey;
 $radius: 4px;
 .alen-button {
   box-sizing: border-box;
@@ -48,6 +59,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   & + & {
     margin-left: 8px;
   }
@@ -78,15 +90,77 @@ $radius: 4px;
       background: darken(white, 5%);;
     }
   }
-  &.gulu-size-big {
+  &.alen-size-big {
     font-size: 24px;
     height: 48px;
     padding: 0 16px;
   }
-  &.gulu-size-small {
+  &.alen-size-small {
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  &.alen-theme-button {
+    &.alen-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.alen-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.alen-theme-link {
+    &.alen-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.alen-theme-text {
+    &.alen-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.alen-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.alen-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.alen-theme-link, &.alen-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
   }
 }
 </style>
