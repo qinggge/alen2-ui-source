ec4fcab4105054c469fa3c333eb15335e818eb87
zhu <zhu_huang@126.com>
2020-09-24 02:28:44 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -6,7 +6,7 @@
       v-for="(t,index) in titles"
       :key="index"
       :class="{selected: t === selected}"
-      :ref="el => { if (el) navItems[index] = el }"
+      :ref="el => { if (t === selected) selectedItem = el }"
       @click="select(t)"
     >{{t}}</div>
     <div class="alen-tabs-nav-indicator" ref="indicator"></div>
@@ -29,16 +29,14 @@ export default {
     },
   },
   setup(props, context) {
-    const navItems = ref<HTMLDivElement[]>([]);
+    const selectedItem = ref<HTMLDivElement>(null);
     const indicator = ref<HTMLDivElement>(null);
     const container = ref<HTMLDivElement>(null);
     const x = () => {
-      const divs = navItems.value;
-      const result = divs.filter(div => div.classList.contains('selected'))[0];
-      const {width} = result.getBoundingClientRect();
+      const {width} = selectedItem.value.getBoundingClientRect();
       indicator.value.style.width = width + 'px';
       const {left: left1} = container.value.getBoundingClientRect();
-      const {left: left2} = result.getBoundingClientRect();
+      const {left: left2} = selectedItem.value.getBoundingClientRect();
       const left = left2 - left1;
       indicator.value.style.left = left + 'px';
     }
@@ -62,7 +60,7 @@ export default {
       context.emit('update:selected', title)
     };
     return {
-      defaults, titles, current, select, navItems, indicator, container,
+      defaults, titles, current, select, indicator, container, selectedItem,
     };
   }
 }
