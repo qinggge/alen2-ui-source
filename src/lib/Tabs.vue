72cad8413c90e950062a932deae669c2af036e3e
zhu <zhu_huang@126.com>
2020-09-24 01:47:22 +0800

    Tabs css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,9 +1,13 @@
 <template>
-  <div>
-    <div v-for="(title, index) in titles" :key="index">{{title}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index" />
+<div class="alen-tabs">
+  <div class="alen-tabs-nav">
+    <div class="alen-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+  </div>
+  <div class="alen-tabs-content">
+    <component class="alen-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
   </div>
 </template>
+
 <script>
 import Tab from './Tab.vue';
 export default {
@@ -23,3 +27,30 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.alen-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
